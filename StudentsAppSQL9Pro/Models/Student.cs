531b47cd0089a10a60dc13df989020395b93f93b
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-26 18:20:56 +0200

    chore: add DAO insert

---
+++ StudentsAppSQL9Pro/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentsAppSQL9Pro.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+    }
+}
