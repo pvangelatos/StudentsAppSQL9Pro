f58d4379191de7dceb2b50df86d8866031c60eaa
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-27 09:53:58 +0200

    chore: add exception for student not found

---
+++ StudentsAppSQL9Pro/Exceptions/StudentNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentsAppSQL9Pro.Exceptions
+{
+    public class StudentNotFoundException : Exception
+    {
+
+        public StudentNotFoundException() : base("Student not found.")
+        {
+        }
+
+        public StudentNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public StudentNotFoundException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
