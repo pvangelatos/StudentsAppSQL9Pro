d103a05d8173cbc4d58683bb9a75d156a037650a
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-27 09:54:20 +0200

    chore: add mapper

---
+++ StudentsAppSQL9Pro/Configuration/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using StudentsAppSQL9Pro.DTO;
+using StudentsAppSQL9Pro.Models;
+
+namespace StudentsAppSQL9Pro.Configuration
+{
+    public class MapperConfig : Profile
+    {
+
+        public MapperConfig()
+        {
+            CreateMap<StudentInsertDTO, Student>().ReverseMap();
+            CreateMap<StudentUpdateDTO, Student>().ReverseMap();
+            CreateMap<StudentReadOnlyDTO, Student>().ReverseMap();
+        }
+    }
+}
